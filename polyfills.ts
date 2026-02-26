2a5076352a95338a6a0393ab00f31632eb99ae70
Thomas Gaugain <thomasgaugain@MacBook-Pro-de-Thomas.local>
2026-02-26 13:18:55 -0700

    wip arcium implementation

--- polyfills.ts
+++ polyfills.ts
@@ -1,5 +1,11 @@
-const { Buffer } = require('buffer');
-globalThis.Buffer = Buffer;
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
+
+if (typeof global.TextEncoder === 'undefined') {
+  require('text-encoding-polyfill');
+}
 
 require('react-native-get-random-values');
 require('react-native-url-polyfill/auto');
